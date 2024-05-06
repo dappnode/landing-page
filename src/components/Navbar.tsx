d02e5668796d78858d6ac7521a7f5d67f5a44eda
pablomendezroyo <mendez4a@gmail.com>
2024-05-06 10:49:13 +0200

    add responsiveness

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { FaMoon } from "react-icons/fa";
+import React, { useState } from "react";
+import { FaMoon, FaBars } from "react-icons/fa";
 import { MdSunny } from "react-icons/md";
 
 interface NavbarProps {
@@ -18,32 +18,79 @@ const pages: {
 ];
 
 const Navbar: React.FC<NavbarProps> = ({ onThemeToggle, theme }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <nav className="flex justify-between items-center border-b h-28 p-4 md:p-6 bg-dappnodeBgLightHeaderFooter dark:bg-dappnodeBgDarkHeaderFooter">
-      <div className="flex items-center">
-        <img src="/dappnode-logo-only.png" alt="Logo" className="h-16 mr-4" />
-        <span className="text-2xl font-bold text-gray-900 dark:text-white">
-          Landing Page
-        </span>
-      </div>
-      <div className="flex gap-4">
-        {pages.map((page, index) => (
-          <a
-            key={index}
-            href={page.path}
-            className="text-2xl text-gray-900 hover:text-dappnodePurple dark:text-white dark:hover:text-dappnodePurple"
+    <div className="relative">
+      <nav className="flex items-center justify-between border-b p-4 md:p-6 bg-dappnodeBgLightHeaderFooter dark:bg-dappnodeBgDarkHeaderFooter z-10">
+        <div className="flex items-center justify-start">
+          <img
+            src="/dappnode-logo-only.png"
+            alt="Logo"
+            className="h-10 md:h-12 mr-4"
+          />
+          <span className="text-xl md:text-2xl font-bold text-gray-900 dark:text-white">
+            Landing Page
+          </span>
+        </div>
+
+        <div className="flex-grow">
+          <div className="hidden md:flex justify-center items-center gap-4">
+            {pages.map((page, index) => (
+              <a
+                key={index}
+                href={page.path}
+                className="text-lg md:text-xl text-gray-900 hover:text-dappnodePurple dark:text-white dark:hover:text-dappnodePurple"
+              >
+                {page.name}
+              </a>
+            ))}
+          </div>
+        </div>
+
+        <div className="flex items-center">
+          <button
+            onClick={toggleMenu}
+            className={`md:hidden p-2 text-xl ${
+              theme === "light" ? "text-gray-800" : "text-white"
+            }`}
           >
-            {page.name}
-          </a>
-        ))}
-      </div>
-      <button
-        onClick={onThemeToggle}
-        className="text-2xl rounded-full p-2 transition-all ease-in-out hover:bg-gray-200 dark:hover:bg-gray-800 text-gray-600 dark:text-gray-300"
-      >
-        {theme === "light" ? <FaMoon /> : <MdSunny />}
-      </button>
-    </nav>
+            <FaBars />
+          </button>
+          <button
+            onClick={onThemeToggle}
+            className="hidden md:inline-block text-xl rounded-full p-2 transition-all ease-in-out hover:bg-gray-200 dark:hover:bg-gray-800 text-gray-600 dark:text-gray-300"
+          >
+            {theme === "light" ? <FaMoon /> : <MdSunny />}
+          </button>
+        </div>
+      </nav>
+      {isMenuOpen && (
+        <div className="absolute top-full inset-x-0 p-4 bg-dappnodeBgLightHeaderFooter dark:bg-dappnodeBgDarkHeaderFooter md:hidden z-0">
+          <div className="flex flex-col items-center gap-4">
+            {pages.map((page, index) => (
+              <a
+                key={index}
+                href={page.path}
+                className="text-lg text-gray-900 hover:text-dappnodePurple dark:text-white dark:hover:text-dappnodePurple"
+              >
+                {page.name}
+              </a>
+            ))}
+            <button
+              onClick={onThemeToggle}
+              className="text-xl rounded-full p-2 transition-all ease-in-out hover:bg-gray-200 dark:hover:bg-gray-800 text-gray-600 dark:text-gray-300"
+            >
+              {theme === "light" ? <FaMoon /> : <MdSunny />}
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
   );
 };
 
