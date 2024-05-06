cc1541d922a417aee7f816e23d399f72c23a94b1
pablomendezroyo <41727368+pablomendezroyo@users.noreply.github.com>
2024-05-06 16:13:38 +0200

    trigger gha pages (#1)
    * trigger gha pages
    
    * update branches

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from "react";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import { FiArrowUp } from "react-icons/fi";
-import About from "./components/About";
 import "./App.css";
+import About from "./components/About";
 
 const App: React.FC = () => {
   const [theme, setTheme] = useState("light");
